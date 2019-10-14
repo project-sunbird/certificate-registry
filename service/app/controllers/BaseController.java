f1f262e18ef04aeb9c67ed9c051d58448d0f3c98
anmol2302 <anmolgupta23021997@gmail.com>
2019-10-14 12:23:33 +0530

    health api response changed

--- service/app/controllers/BaseController.java
+++ service/app/controllers/BaseController.java
@@ -119,7 +119,9 @@ public class BaseController extends Controller {
 
     public CompletionStage<Result> handleRequest() {
         CompletableFuture<String> cf = new CompletableFuture<>();
-        cf.complete("helloOk");
+         String dummyResponse =
+                "{\"id\":\"api.service.health\",\"ver\":\"v1\",\"ts\":\"2019-01-17 16:53:26:286+0530\",\"params\":{\"resmsgid\":null,\"msgid\":\"8e27cbf5-e299-43b0-bca7-8347f7ejk5abcf\",\"err\":null,\"status\":\"success\",\"errmsg\":null},\"responseCode\":\"OK\",\"result\":{\"response\":{\"response\":\"SUCCESS\",\"errors\":[]}}}";
+        cf.complete(dummyResponse);
         return cf.thenApplyAsync(Results::ok);
     }
 
