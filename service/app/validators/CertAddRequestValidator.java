4bd35463c6060bfe3c0a19f9af6defd0bb027ff3
amit <amit.kumar@tarento.com>
2020-01-03 14:07:44 +0530

    Issue #SC-1604 feat: response message fix for request validator

--- service/app/validators/CertAddRequestValidator.java
+++ service/app/validators/CertAddRequestValidator.java
@@ -7,11 +7,13 @@ import org.apache.log4j.Logger;
 import org.sunbird.BaseException;
 import org.sunbird.JsonKeys;
 import org.sunbird.message.IResponseMessage;
+import org.sunbird.message.Localizer;
 import org.sunbird.message.ResponseCode;
 import org.sunbird.request.Request;
 
 import java.text.MessageFormat;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 /**
@@ -21,6 +23,7 @@ import java.util.Map;
 public class CertAddRequestValidator implements IRequestValidator {
     static Logger logger=Logger.getLogger(CertAddRequestValidator.class);
     private Request request;
+    private Localizer localizer = Localizer.getInstance();
     static List<String> mandatoryParamsList = Lists.newArrayList(JsonKeys.ID, JsonKeys.ACCESS_CODE, JsonKeys.PDF_URL);
     static List<String> allowedRecipientsType=Lists.newArrayList(JsonKeys.INDIVIDUAL,JsonKeys.ENTITY);
     public CertAddRequestValidator() {
@@ -44,14 +47,14 @@ public class CertAddRequestValidator implements IRequestValidator {
     private void validateMandatoryJsonData() throws BaseException {
         if(MapUtils.isEmpty((Map)request.getRequest().get(JsonKeys.JSON_DATA))){
             logger.error("CertAddRequestValidator:validateMandatoryJsonData:incorrect request provided");
-            throw new BaseException(IResponseMessage.INVALID_REQUESTED_DATA, MessageFormat.format(IResponseMessage.EMPTY_MANDATORY_PARAM,JsonKeys.JSON_DATA), ResponseCode.CLIENT_ERROR.getCode());
+            throw new BaseException(IResponseMessage.INVALID_REQUESTED_DATA, MessageFormat.format(getLocalizedMessage(IResponseMessage.EMPTY_MANDATORY_PARAM,null),JsonKeys.JSON_DATA), ResponseCode.CLIENT_ERROR.getCode());
         }
         validateDataType();
     }
     private void validateDataType() throws BaseException {
         if (!(request.get(JsonKeys.JSON_DATA) instanceof Map)) {
             logger.error("CertAddRequestValidator:validateDataType:incorrect request provided");
-            throw new BaseException(IResponseMessage.INVALID_REQUESTED_DATA, MessageFormat.format(IResponseMessage.DATA_TYPE_ERROR,JsonKeys.JSON_DATA,"map"), ResponseCode.CLIENT_ERROR.getCode());
+            throw new BaseException(IResponseMessage.INVALID_REQUESTED_DATA, MessageFormat.format(getLocalizedMessage(IResponseMessage.DATA_TYPE_ERROR,null),JsonKeys.JSON_DATA,"map"), ResponseCode.CLIENT_ERROR.getCode());
 
         }
     }
@@ -60,16 +63,16 @@ public class CertAddRequestValidator implements IRequestValidator {
         Map<String,Object>certAddReqMap=request.getRequest();
         if(MapUtils.isEmpty(request.getRequest())){
             logger.error("CertAddRequestValidator:validateMandatoryParams:incorrect request provided");
-            throw new BaseException(IResponseMessage.INVALID_REQUESTED_DATA, IResponseMessage.INVALID_REQUESTED_DATA, ResponseCode.CLIENT_ERROR.getCode());
+            throw new BaseException(IResponseMessage.INVALID_REQUESTED_DATA, getLocalizedMessage(IResponseMessage.INVALID_REQUESTED_DATA,null), ResponseCode.CLIENT_ERROR.getCode());
         }
         for (String param :mandatoryParamsList) {
             if(!certAddReqMap.containsKey(param)){
                 logger.error("CertAddRequestValidator:validateMandatoryParams:incorrect request provided");
-                throw new BaseException(IResponseMessage.INVALID_REQUESTED_DATA, MessageFormat.format(IResponseMessage.MISSING_MANDATORY_PARAMS,param), ResponseCode.CLIENT_ERROR.getCode());
+                throw new BaseException(IResponseMessage.INVALID_REQUESTED_DATA, MessageFormat.format(getLocalizedMessage(IResponseMessage.MISSING_MANDATORY_PARAMS,null),param), ResponseCode.CLIENT_ERROR.getCode());
             }
             if(!(certAddReqMap.get(param) instanceof String)){
                 logger.error("CertAddRequestValidator:validateMandatoryParams:incorrect request provided");
-                throw new BaseException(IResponseMessage.INVALID_REQUESTED_DATA, MessageFormat.format(IResponseMessage.DATA_TYPE_ERROR,param,"string"), ResponseCode.CLIENT_ERROR.getCode());
+                throw new BaseException(IResponseMessage.INVALID_REQUESTED_DATA, MessageFormat.format(getLocalizedMessage(IResponseMessage.DATA_TYPE_ERROR,null),param,"string"), ResponseCode.CLIENT_ERROR.getCode());
             }
             validatePresence(param,(String)certAddReqMap.get(param));
         }
@@ -78,22 +81,22 @@ public class CertAddRequestValidator implements IRequestValidator {
     private void validatePresence(String key,String value) throws BaseException {
         if (StringUtils.isBlank(value)) {
             logger.error("CertAddRequestValidator:validatePresence:incorrect request provided");
-            throw new BaseException(IResponseMessage.INVALID_REQUESTED_DATA, MessageFormat.format(IResponseMessage.EMPTY_MANDATORY_PARAM,key), ResponseCode.CLIENT_ERROR.getCode());
+            throw new BaseException(IResponseMessage.INVALID_REQUESTED_DATA, MessageFormat.format(getLocalizedMessage(IResponseMessage.EMPTY_MANDATORY_PARAM,null),key), ResponseCode.CLIENT_ERROR.getCode());
         }
     }
 
 
     private void validateRelatedObject() throws BaseException {
         if(!(request.getRequest().get(JsonKeys.RELATED) instanceof Map)){
-            throw new BaseException(IResponseMessage.INVALID_REQUESTED_DATA, MessageFormat.format(IResponseMessage.DATA_TYPE_ERROR,JsonKeys.RELATED,"map"), ResponseCode.CLIENT_ERROR.getCode());
+            throw new BaseException(IResponseMessage.INVALID_REQUESTED_DATA, MessageFormat.format(getLocalizedMessage(IResponseMessage.DATA_TYPE_ERROR,null),JsonKeys.RELATED,"map"), ResponseCode.CLIENT_ERROR.getCode());
         }
         Map<String,Object>relatedMap=(Map)request.getRequest().get(JsonKeys.RELATED);
         if(!relatedMap.containsKey(JsonKeys.TYPE)){
-            throw new BaseException(IResponseMessage.INVALID_REQUESTED_DATA, MessageFormat.format(IResponseMessage.MISSING_MANDATORY_PARAMS, JsonKeys.TYPE.concat(" inside related map")), ResponseCode.CLIENT_ERROR.getCode());
+            throw new BaseException(IResponseMessage.INVALID_REQUESTED_DATA, MessageFormat.format(getLocalizedMessage(IResponseMessage.MISSING_MANDATORY_PARAMS,null), JsonKeys.TYPE.concat(" inside related map")), ResponseCode.CLIENT_ERROR.getCode());
         }
         String relatedType=(String)relatedMap.get(JsonKeys.TYPE);
         if(StringUtils.isBlank(relatedType)){
-            throw new BaseException(IResponseMessage.INVALID_REQUESTED_DATA,IResponseMessage.INVALID_RELATED_TYPE, ResponseCode.CLIENT_ERROR.getCode());
+            throw new BaseException(IResponseMessage.INVALID_REQUESTED_DATA,getLocalizedMessage(IResponseMessage.INVALID_RELATED_TYPE,null), ResponseCode.CLIENT_ERROR.getCode());
         }
     }
 
@@ -101,7 +104,11 @@ public class CertAddRequestValidator implements IRequestValidator {
 
         String recType=(String)request.getRequest().get(JsonKeys.RECIPIENT_TYPE);
         if(!allowedRecipientsType.contains(recType)){
-            throw new BaseException(IResponseMessage.INVALID_REQUESTED_DATA, MessageFormat.format(IResponseMessage.INVALID_RECIPIENT_TYPE,recType,allowedRecipientsType), ResponseCode.CLIENT_ERROR.getCode());
+            throw new BaseException(IResponseMessage.INVALID_REQUESTED_DATA, MessageFormat.format(getLocalizedMessage(IResponseMessage.INVALID_RECIPIENT_TYPE,null),recType,allowedRecipientsType), ResponseCode.CLIENT_ERROR.getCode());
         }
+    }
+
+    private String getLocalizedMessage(String key, Locale locale){
+        return localizer.getMessage(key, locale);
     }
 }
--- service/app/validators/CertDownloadRequestValidator.java
+++ service/app/validators/CertDownloadRequestValidator.java
@@ -4,10 +4,12 @@ import org.apache.commons.lang3.StringUtils;
 import org.sunbird.BaseException;
 import org.sunbird.JsonKeys;
 import org.sunbird.message.IResponseMessage;
+import org.sunbird.message.Localizer;
 import org.sunbird.message.ResponseCode;
 import org.sunbird.request.Request;
 
 import java.text.MessageFormat;
+import java.util.Locale;
 
 /**
  * this is a validator class for downloading certificate
@@ -15,6 +17,7 @@ import java.text.MessageFormat;
  */
 public class CertDownloadRequestValidator implements IRequestValidator {
 
+    private Localizer localizer = Localizer.getInstance();
     private Request request;
     @Override
     public void validate(Request request) throws BaseException {
@@ -26,9 +29,11 @@ public class CertDownloadRequestValidator implements IRequestValidator {
 
     private void validateDownlaodFileData() throws BaseException {
         if (StringUtils.isBlank((String)this.request.getRequest().get("pdfUrl"))) {
-            throw new BaseException(IResponseMessage.INVALID_REQUESTED_DATA, MessageFormat.format(IResponseMessage.MISSING_MANDATORY_PARAMS, JsonKeys.PDF_URL), ResponseCode.CLIENT_ERROR.getCode());
+            throw new BaseException(IResponseMessage.INVALID_REQUESTED_DATA, MessageFormat.format(getLocalizedMessage(IResponseMessage.MISSING_MANDATORY_PARAMS,null), JsonKeys.PDF_URL), ResponseCode.CLIENT_ERROR.getCode());
         }
     }
 
-
+    private String getLocalizedMessage(String key, Locale locale){
+        return localizer.getMessage(key, locale);
+    }
 }
