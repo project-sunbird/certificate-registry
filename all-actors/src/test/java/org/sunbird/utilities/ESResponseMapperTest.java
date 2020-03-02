a2b4308f64ea49efb4dffbc469a7895d538a2706
anmol2302 <anmolgupta23021997@gmail.com>
2020-03-02 16:32:59 +0530

    content mapper test cases written

--- all-actors/src/test/java/org/sunbird/utilities/ESResponseMapperTest.java
+++ all-actors/src/test/java/org/sunbird/utilities/ESResponseMapperTest.java
@@ -1,6 +1,7 @@
 package org.sunbird.utilities;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -10,11 +11,19 @@ import static org.junit.Assert.assertEquals;
 public class ESResponseMapperTest {
 
     @Test
-    public void whenDeserializingUsingJsonCreator_thenCorrect() throws IOException {
+    public void mappingTotalWithCountSuccess() throws IOException {
         String json = "{\"total\":1}";
         ESResponseMapper bean = new ObjectMapper()
                 .readerFor(ESResponseMapper.class)
                 .readValue(json);
         assertEquals(1, bean.getCount());
+    }
+    @Test
+    public void mappingHitsWithContentSuccess() throws IOException {
+        String json = "{\"content\": [{ \"_type\": \"_doc\"}]}";
+        ESResponseMapper bean = new ObjectMapper()
+                .readerFor(ESResponseMapper.class)
+                .readValue(json);
+        Assert.assertNotNull(bean.getContent());
     }
 }
